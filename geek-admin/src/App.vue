00d2a3b38bdc198e7899f4d42b26acb8ea495cec
wangwenpeng <wangwp1218@digiwin.com>
2023-01-02 16:08:52 +0800

    init project

---
+++ geek-admin/src/App.vue
@@ -0,0 +1,19 @@
+
+<template>
+  <div>
+    <router-link to="/">首页</router-link> | 
+    <router-link to="/about">关于</router-link>
+  </div>
+  <router-view></router-view>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  name: 'App',
+  components: {
+    HelloWorld
+  }
+}
+</script>
